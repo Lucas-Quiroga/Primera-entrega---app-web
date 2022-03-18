15f920f988cb30f9df800d29229f929ec21e562b
Lucas Quiroga <quiroga.lucasoffice@gmail.com>
2022-03-18 16:22:17 -0300

    mejorando la main

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-//En esta primer entrega me centre más en la parte de productos, ya que voy a trabajar con con carrito//
-
 //arrays//
 const insumos = [];
-const servicios = []
+const servicios = [];
 
 //objetos//
 class Insumos{
@@ -58,67 +56,115 @@ agregueServicios(2, "Reiki", 1100, "referencia", "30 - 60min");
 agregueServicios(3, "Sanación", 700, "referencia", "10 - 20min");
 agregueServicios(4, "Limpieza energética", 1100, "referencia", "10 - 20min");
 
-//Preguntamos si quiere ver el catalogo de productos//
-let askClientIms = prompt("¿Ver detalles de productos?");
+//DOM
+let sectionProductos = document.getElementById("productos");
 
-function catalogo(){
-    if(askClientIms === "Si"){
-    for (const productos of insumos)
-        alert(`Nombre del producto: ${productos.producto}\nPrecio: ${productos.precio}`);
-    } else {
-        alert("No hay problema, saludos!")
-    }
+function crearCard (insumos) {
+    //primero creo el div contenedor para cada card y aplico estilos de bootstrap
+    let card = document.createElement("div");
+    card.setAttribute("class", "card card-producto");
+    card.style.width = "18rem";
+    //le asigno las cards al elemento padre section
+    sectionProductos.appendChild(card);
+    //creo divs que contengan las imagenes de los productos y se los asigno como hijos a las cards
+    let cardImg = document.createElement("div");
+    cardImg.innerHTML = `<img src="./img/${insumos.id}.jpg" class="card-img-top" alt="${insumos.producto}"></img>`;
+    card.appendChild(cardImg);
+    //creo divs que contengan los nombres de los productos y los botones, despues se los asigno como hijos a las cards
+    let cardBody = document.createElement("div");
+    cardBody.innerHTML = `<h5 class="card-title">${insumos.producto}</h5>
+                            <p class="card-text"> Precio: $${insumos.precio} </p>
+                            <button class="btn btn-dark">Comprar</button>`;
+    card.appendChild(cardBody);
+    cardBody.setAttribute("class", "card-body");
 }
 
-//llamamos a la función para mostrarle los productos al usuario//
-catalogo();
-
-//Preguntamos si quiere ver los servicios//
-let askClientSer = prompt("¿Ver detalles de los servicios ofrecidos?");
-
-function catalogoSer(){
-    if(askClientSer === "Si"){
-    for (const productos of servicios)
-    alert(`Nombre del servicio: ${productos.servicio}\nValor: ${productos.precio}\nInfo: ${productos.detalle}\nDuración: ${productos.duracion}`);
-    } else {
-        alert("No hay problema, saludos!")
-    }
+let serviciosProductos = document.getElementById("Servicios");
+function crearCard2 (servicios) {
+    let card2 = document.createElement("div");
+    card2.setAttribute("class", "card card-producto");
+    card2.style.width = "18rem";
+    serviciosProductos.appendChild(card2);
+    let cardImg2 = document.createElement("div");
+    cardImg2.innerHTML = `<img src="./imgS/${servicios.id}.jpeg" class="card-img-top" alt="${servicios.producto}"></img>`;
+    card2.appendChild(cardImg2);
+    let cardBody2 = document.createElement("div");
+    cardBody2.innerHTML = `<h5 class="card-title">${servicios.servicio}</h5>
+    <p class="card-text"> Precio: $${servicios.precio} </p>
+    <button class="btn btn-dark">Comprar</button>`;
+    card2.appendChild(cardBody2);
+    cardBody2.setAttribute("class", "card-body");
 }
 
-//Llamamos a la función para mostrarle los servicios al usuario//
-catalogoSer();
-
-//Funcion para calcular cantidad y producto
-function calcularProductos(precioProducto, cantidadProducto ){
-    return precioProducto * cantidadProducto
+for (const insumo of insumos) {
+   crearCard(insumo);
 }
-let guardar = calcularProductos;
 
-//Le preguntamos al usuario que producto quiere//
-let productoSeleccionado = prompt("Que producto quiere?");
-let productoSeleccionadoCant = parseInt(prompt("Cuanta cantidad desea?"));
-const productoBusqueda = insumos.find(product => product.producto === productoSeleccionado);
-
-switch (productoSeleccionado){
-    case "Velas":
-        alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
-        productoBusqueda.vender;
-        alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
-        break
-    case "Collares":
-        alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
-        productoBusqueda.vender;
-        alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
-        break
-    case "Sahumerios":
-        alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
-        productoBusqueda.vender;
-        alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
-        break
-    default:
-        alert("Por favor ingrese un producto valido/disponible")
-        break;
+for (const servicio of servicios){
+    crearCard2(servicio)
 }
+
+// //Preguntamos si quiere ver el catalogo de productos//
+// let askClientIms = prompt("¿Ver detalles de productos?");
+
+// function catalogo(){
+//     if(askClientIms === "Si"){
+//     for (const productos of insumos)
+//         alert(`Nombre del producto: ${productos.producto}\nPrecio: ${productos.precio}`);
+//     } else {
+//         alert("No hay problema, saludos!")
+//     }
+// }
+
+// //llamamos a la función para mostrarle los productos al usuario//
+// catalogo();
+
+// //Preguntamos si quiere ver los servicios//
+// let askClientSer = prompt("¿Ver detalles de los servicios ofrecidos?");
+
+// function catalogoSer(){
+//     if(askClientSer === "Si"){
+//     for (const productos of servicios)
+//     alert(`Nombre del servicio: ${productos.servicio}\nValor: ${productos.precio}\nInfo: ${productos.detalle}\nDuración: ${productos.duracion}`);
+//     } else {
+//         alert("No hay problema, saludos!")
+//     }
+// }
+
+// //Llamamos a la función para mostrarle los servicios al usuario//
+// catalogoSer();
+
+// //Funcion para calcular cantidad y producto
+// function calcularProductos(precioProducto, cantidadProducto ){
+//     return precioProducto * cantidadProducto
+// }
+// let guardar = calcularProductos;
+
+// //Le preguntamos al usuario que producto quiere//
+// let productoSeleccionado = prompt("Que producto quiere?");
+// let productoSeleccionadoCant = parseInt(prompt("Cuanta cantidad desea?"));
+// const productoBusqueda = insumos.find(product => product.producto === productoSeleccionado);
+
+// switch (productoSeleccionado){
+//     case "Velas":
+//         alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
+//         productoBusqueda.vender;
+//         alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
+//         break
+//     case "Collares":
+//         alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
+//         productoBusqueda.vender;
+//         alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
+//         break
+//     case "Sahumerios":
+//         alert ("El precio final de su producto es: $" + calcularProductos(productoBusqueda.precio, productoSeleccionadoCant));
+//         productoBusqueda.vender;
+//         alert("Muchas gracias, para retirarlo comunicarse al +12 12345678")
+//         break
+//     default:
+//         alert("Por favor ingrese un producto valido/disponible")
+//         break;
+// }
 
 //funcion que modifica los objetos en el array para aumentarles el iva sin cambiar de array//
 function infoInsumos(){
