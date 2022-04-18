4b1b0aa9dc9cf5d0366b8d23b9658d981e7b2b39
Lucas Quiroga <quiroga.lucasoffice@gmail.com>
2022-04-17 23:17:50 -0300

    cambiosFetch

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -1,3 +1,11 @@
+
+// traemos la funcion para usarla en el carrito
+import {returnProducts} from "./fetch.js"
+
+// exportamos
+export{insumosElegidos}
+export{meterAlCarro}
+
 //inicializamos array de carrito
 let carrito = [];
 let insumosElegidos = document.getElementById("sec-insumos");
@@ -93,6 +101,33 @@ function sumaDeTotalCarrito(){
     precioTotal.innerText = total;
     insumosOrigen.innerText = carrito.length;
 }
+
+const returnProducts = async () => {
+    const insumos = await fetch("/insumos.json")
+        const data = await insumos.json()
+            data.forEach(product => {
+                let listElegidos = document.getElementById("listado");
+                const list = document.createElement("div");
+                list.setAttribute("class", "card card-producto");
+                list.style.width = "18rem";
+                list.innerHTML = `<div class="img-container">
+                                  <h5 class="card-title">${product.nombre}</h5>
+                                  <img src="${product.foto}" alt="${product.nombre}</p>
+                                  ${product.nombre}
+                                  <p class="card-text"> Precio:</p>
+                                  <strong class="font">$${product.precio}</strong>
+                                  <br>
+                                  <button type="button" class="btn btn-dark">Agregar</button>
+                                  </div>`;
+                                  listElegidos.appendChild(list);
+                                  insumosElegidos.appendChild(listElegidos);
+                                  document.getElementById(`${product.id}`).onclick = () => meterAlCarro(`${product.id}`);
+            });
+        };
+returnProducts();
+
+
+
 
 //recorremos con un for los insumos//
 
--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,20 +1,27 @@
-let listElegidos = document.getElementById("listado");
-const returnProducts = async () => {
-const insumos = await fetch("/insumos.json")
-    const data = await insumos.json()
-        data.forEach(product => {
-            const list = document.createElement("div");
-            list.setAttribute("class", "card card-producto");
-            list.style.width = "18rem";
-            list.innerHTML = `<div class="img-container">
-                              <img src="${product.foto}" alt="${product.nombre}</p>
-                              ${product.nombre}
-                              <strong class="font">$${product.precio}</strong>
-                              <br>
-                              <button class= "boton" id="${product.id}"> Agregar </button>
-                              </div>`;
-                              listElegidos.appendChild(list);
-        });
-    };
-//La concha de tu madre ollboy
-  returnProducts();
+import {insumosElegidos} from "./carrito.js"
+import {meterAlCarro} from "./carrito.js"
+
+// const returnProducts = async () => {
+// const insumos = await fetch("/insumos.json")
+//     const data = await insumos.json()
+//         data.forEach(product => {
+//             let listElegidos = document.getElementById("listado");
+//             const list = document.createElement("div");
+//             list.setAttribute("class", "card card-producto");
+//             list.style.width = "18rem";
+//             list.innerHTML = `<div class="img-container">
+//                               <h5 class="card-title">${product.nombre}</h5>
+//                               <img src="${product.foto}" alt="${product.nombre}</p>
+//                               ${product.nombre}
+//                               <p class="card-text"> Precio:</p>
+//                               <strong class="font">$${product.precio}</strong>
+//                               <br>
+//                               <button type="button" class="btn btn-dark">Agregar</button>
+//                               </div>`;
+//                               listElegidos.appendChild(list);
+//                               insumosElegidos.appendChild(listElegidos);
+//                               document.getElementById(`${product.id}`).onclick = () => meterAlCarro(`${product.id}`);
+//         });
+//     };
+
+  export {returnProducts}
