eed75b8e2f57d4e9345d228948fcd817eb1719da
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-04-21 14:32:13 -0300

    malasPracticas

--- cleanCode/discart.js
+++ cleanCode/discart.js
@@ -71,6 +71,31 @@
     // let botonprobando=document.getElementById("probando");
     // botonprobando.addEventListener("click", ()=>{alert("Funciona")});
 
+//Cargue el fetch local para crear las cards asi podes tener mas rapido los datos
+// let listElegidos = document.getElementById("listado");
+// const returnProducts = async () => {
+// const insumos = await fetch("/insumos.json")
+//     const data = await insumos.json()
+//         data.forEach(product => {
+//             const list = document.createElement("div");
+//             list.setAttribute("class", "card card-producto");
+//             list.style.width = "18rem";
+//             list.innerHTML = `<div class="img-container">
+//                               <h5 class="card-title">${product.nombre}</h5>
+//                               <img src="${product.foto}" alt="${product.nombre}</p>
+//                               ${product.nombre}
+//                               <p class="card-text"> Precio:</p>
+//                               <strong class="font">$${product.precio}</strong>
+//                               <br>
+//                               <button type="button" class="btn btn-dark">Agregar</button>
+//                               </div>`;
+//                               listElegidos.appendChild(list);
+//                               // insumosElegidos.appendChild(listElegidos);
+//                               // document.getElementById(`${insumos.id}`).onclick = () => meterAlCarro(`${insumos.id}`);
+//         });
+//     };
+//      returnProducts();
+
 /*--------------------------------------------------------------------------------------------------------------------------------*/
 
 // <!-- insumos.js -->
--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,45 +1,39 @@
-//Cargue el fetch local para crear las cards asi podes tener mas rapido los datos
-let listElegidos = document.getElementById("listado");
-const returnProducts = async () => {
-const insumos = await fetch("/insumos.json")
-    const data = await insumos.json()
-        data.forEach(product => {
-            const list = document.createElement("div");
-            list.setAttribute("class", "card card-producto");
-            list.style.width = "18rem";
-            list.innerHTML = `<div class="img-container">
-                              <h5 class="card-title">${product.nombre}</h5>
-                              <img src="${product.foto}" alt="${product.nombre}</p>
-                              ${product.nombre}
-                              <p class="card-text"> Precio:</p>
-                              <strong class="font">$${product.precio}</strong>
-                              <br>
-                              <button type="button" class="btn btn-dark">Agregar</button>
-                              </div>`;
-                              listElegidos.appendChild(list);
-                              // insumosElegidos.appendChild(listElegidos);
-                              // document.getElementById(`${insumos.id}`).onclick = () => meterAlCarro(`${insumos.id}`);
-        });
-    };
-     returnProducts();
 
-
-
-     const productos2 = document.createElement("div")
-     async function viendoCard () {
+    const divCards = document.getElementById("cardlist");
+     
+     const viendoCard = async () => {
      const esperandoFetch = await fetch("/insumos.json")
      .then((resp) => resp.json())
      .then((data) => {
+         data.forEach((insumos) => {
         const productos = document.createElement("div")
         productos.setAttribute("class", "card")
-        productos.innerHTML = ` <img class="card-img-top" src="${product.foto}" alt="Card image cap">
+        productos.style.width = "18rem";
+        productos.innerHTML = ` <div class="img-container">
+                                <img class="card-img-top" src="${insumos.foto}" alt="${insumos.nombre}">
                                 <div class="card-body">
-                                <h5 class="card-title">${product.nombre}</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make upthe bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>`
-                                productos2.appendChild(productos);
-     })
-    }
+                                <h5 class="card-title">${insumos.nombre}</h5>
+                                <p class="card-text"> Precio:</p>
+                                <strong class="font">$${insumos.precio}</strong>
+                                <br>
+                                <button type="button" class="btn btn-dark" class="btn" onclick ="Toastify({
 
+                                    text: 'This is a toast',
+                                    
+                                    duration: 3000
+                                    
+                                    }).showToast();">Agregar</button>
+                                </div>
+                                </div>`
+                                divCards.appendChild(productos);
+         })
+        })
+    }
     viendoCard();
+    
+    
+//     const llamarEvento = () => {
+//     let botonCard = document.getElementsByClassName("btn");
+//     botonCard.addEventListener("click", () => {alert("felicidades")});
+// }
+//     llamarEvento()
