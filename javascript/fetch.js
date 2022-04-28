d5736287cc9c02c40d6c531f647297421b5c97eb
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-04-28 15:06:13 -0300

    arreglando modal

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -28,3 +28,10 @@
     // viendoCard();
 
     // export {viendoCard}
+
+const dialog = document.querySelector("dialog");
+const cancel = document.querySelector("#cancel");
+const show = document.querySelector("#show")
+
+show.addEventListener("click", ()=> dialog.showModal())
+cancel.addEventListener("click", ()=> dialog.close())
